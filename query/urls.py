345c931434a2e061f7ad954e82fc569cdf7a0de2
stephanie <bfssi.stephanie.diaz@gmail.com>
2023-06-16 11:30:51 -0400

    home page and base.html

---
+++ query/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, re_path
+# from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    path('', views.QueryView.as_view(), name='home_page'),
+#     path('user/<str:username>', views.UserListView.as_view(), name='user_list'),
+#     path('entry/<int:pk>/', views.EntryDetailView.as_view(), name='entry_detail'),
+#     path('entry/new/', views.EntryCreateView.as_view(), name='entry_create'),
+#     path('entry/<int:pk>/update/', views.EntryUpdateView.as_view(), name='entry_update'),
+#     path('entry/<int:pk>/delete/', views.EntryDeleteView.as_view(), name='entry_delete'),
+#     path('entry/archives/', views.EntryHistoryView.as_view(), name='entry_archives'),
+#     path('entry/archives/<int:pk>/', views.HistoryDetailView.as_view(), name='history_detail'),
+]
--- query/views.py
+++ query/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from api.models import Query
+from cqt_customer_query_tool.users.models import User
+from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 
 # Create your views here.
+
+class QueryView(LoginRequiredMixin, ListView):
+    model = Query
+    template_name = 'Query/home_page.html'
+    context_object_name = 'queries'
+    # paginate_by = 10
+#     order entries from newest to oldest
+    ordering = ['-updated']
